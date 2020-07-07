3eb22fe703d91e3008c63f0e640903b8cbde176b
Julian Ramirez <hipogea@hotmail.com>
2020-07-07 09:50:48 -0500

    actualizando rama_alterna

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\SignupForm */
-
+use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
